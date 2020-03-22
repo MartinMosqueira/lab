ef5c53d03fe92bf1e4665f0b242617bd22720e70
martin <yomartinmosqueira331@gmail.com>
2020-03-22 13:28:37 -0300

    terminado ejercicio 3

---
+++ tps/Tp1/3/app.py
@@ -0,0 +1,5 @@
+def histograma_de_enteros(lista, caracter):
+    lista_caracteres = []
+    for elementos_lista in lista:
+        lista_caracteres.append(caracter*elementos_lista)
+    return lista_caracteres
