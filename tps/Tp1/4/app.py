96715664b6f338e2c02f12c8f1f215f617c8b5c6
MartinMosqueira <yomartinmosqueira331@gmail.com>
2020-06-16 08:01:48 -0300

    Cambio de lugar de tps y tp2 sin terminar
    Se cambiaron de lugar los tps a la carpeta alumnos/ y se subio el tp2
    sin terminar.

--- tps/Tp1/4/app.py
+++
@@ -1,8 +0,0 @@
-def histograma_de_enteros(lista, caracter):
-    archivo = open('ejercicio3.txt', 'a')
-    archivo.write('--------------------')
-    archivo.write('\n')
-    for elementos_lista in lista:
-        archivo.write(caracter*elementos_lista)
-        archivo.write('\n')    
-    archivo.close()
